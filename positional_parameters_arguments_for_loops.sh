f77eb84602b1ba7b670c0071ff39e6e2cf58d0a6
danmarchuk <zemero58@gmail.com>
2022-08-11 17:22:33 +0200

    finished loop script
    Here we added the for loop that generates a pessword for each provided user

--- positional_parameters_arguments_for_loops.sh
+++ positional_parameters_arguments_for_loops.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 echo "You executed this command: ${0}"
+
+# Display the path and the filename of the script.
+echo "You used $(dirname ${0}) as the path to the $(basename ${0}) script."
+
+#Tell them how many arguments they passed in.
+# (inside the script they are parameters, outside they are arguments.)
+NUMBER_OF_PARAMETERS="${#}"
+echo "You supplied ${NUMBER_OF_PARAMETERS} argument(s) on the command line."
+
+# Make sure they at least supply one argument.
+if [[ "${NUMBER_OF_PARAMETERS}" -lt 1 ]]
+then
+	echo "Usage: ${0} USER_NAME [USER_NAME]..."
+	exit 1
+fi
+
+# Generate and display a password for each parameter.
+for USER_NAME in "${@}"
+do
+	PASSWORD=$(date +%s%N | sha256sum)
+	echo "${USER_NAME}: ${PASSWORD}"
+done
